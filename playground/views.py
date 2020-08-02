23c35d3d821838cba3c1e40be97772c3dc797a77
bojbrook <bowenjbrooks@gmail.com>
2020-08-02 11:32:41 -0700

    updated the structuring for the project

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import logging
+
+from ddtrace import tracer
 # from pythonjsonlogger import jsonlogger
 
 # initializing logging
@@ -13,8 +15,12 @@ import logging
 # formatter = jsonlogger.JsonFormatter()
 # logHandler.setFormatter(formatter)
 # logger.addHandler(logHandler)
+@tracer.wrap()
+def hello_world():
+  print("Hello World")
 
 
 
 def index(request):
+  hello_world()
   return HttpResponse("Hello, world!!")
