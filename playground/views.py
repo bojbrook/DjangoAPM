6e3722ccf7d895efe71345d38790c155ae3ccf62
bojbrook <bowenjbrooks@gmail.com>
2020-09-01 11:50:43 -0700

    Added logging

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import logging
+import json_log_formatter
 
 from ddtrace import tracer
-# from pythonjsonlogger import jsonlogger
+# 
 
 # initializing logging
-# formatter = json_log_formatter.JSONFormatter()
-# json_handler = logging.FileHandler(filename='/var/log/my-log.json')
-# json_handler.setFormatter(formatter)
+formatter = json_log_formatter.JSONFormatter()
+json_handler = logging.FileHandler(filename='my-log.json')
+json_handler.setFormatter(formatter)
+
+logger = logging.getLogger('my_json')
+logger.addHandler(json_handler)
+logger.setLevel(logging.INFO)
 # Get an instance of a logger
 
 # logHandler = logging.StreamHandler()
@@ -17,10 +22,12 @@ from ddtrace import tracer
 # logger.addHandler(logHandler)
 @tracer.wrap()
 def hello_world():
+  logger.info("This is a log")
   print("Hello World")
 
 
 
 def index(request):
+  logger.info("Please inject this log")
   hello_world()
   return HttpResponse("Hello, world!!")
